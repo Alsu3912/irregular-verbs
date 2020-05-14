e258c5eb71d853ef55fec6dc602a74f196ba63a7
Alsu3912 <alsu3912@gmail.com>
2020-05-14 21:42:09 +0200

    removed unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import VerbsTable from './component/verb'
 import FrontPage from './component/frontPage'
 import * as serviceWorker from './serviceWorker';
 
