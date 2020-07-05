d64733cafdf8767c2ac673de6630f77b65e903ca
Alsu3912 <alsu3912@gmail.com>
2020-07-05 15:51:24 +0200

    new style was added

---
+++ src/component/siteHeader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SiteHeader(): JSX.Element {
+    return (
+        <div id="site_header">
+            <h3>irregular verbs</h3>
+            <hr>
+            </hr>
+            <nav>
+                <a href="">main page</a>
+                <a href="">learning</a>
+                <a href="">training</a>
+            </nav>
+        </div>
+    )
+}
+
+export default SiteHeader;
