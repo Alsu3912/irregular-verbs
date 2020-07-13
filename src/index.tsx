9e0a33279b3dcb0cda43aa320eddeb08a6097bd4
Alsu3912 <alsu3912@gmail.com>
2020-07-13 15:21:05 +0200

    router was added

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import FrontPage from './component/frontPage'
 import * as serviceWorker from './serviceWorker';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import './index.css';
+import FrontPage from './component/frontPage';
+import LearningPage from './component/learningPage';
+import TrainingPage from './component/trainingPage'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <FrontPage />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={FrontPage} >
+      </Route>
+      <Route exact path="/learning" component={LearningPage} >
+      </Route>
+      <Route exact path="/training" component={TrainingPage} >
+      </Route>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
@@ -15,3 +29,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+{/* <React.StrictMode>
+    <FrontPage />
+  </React.StrictMode>, */}
