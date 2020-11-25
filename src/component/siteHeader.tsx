ee767e39b7e9f001db094f9d5d938592de4b09c4
Alsu3912 <alsu3912@gmail.com>
2020-11-25 19:24:32 +0000

    Link component was replaced by NavLink component

--- src/component/siteHeader.tsx
+++ src/component/siteHeader.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 function SiteHeader(): JSX.Element {
     return (
         <div id="site_header" className="header">
             <Link to="/" className="logo">irregular verbs</Link>
             <nav>
-                <Link to="/" className="active">main</Link>
-                <Link to="/learning">learning</Link>
-                <Link to="/training">training</Link>
+                <NavLink exact activeClassName="active" to="/">main</NavLink>
+                <NavLink exact activeClassName="active" to="/learning">learning</NavLink>
+                <NavLink exact activeClassName="active" to="/training">training</NavLink>
             </nav>
         </div>
     )
